dde7bf41fd4b5b06dfce31ddff8fcf67a1df2709
Suman Thota <suman.thota@gmail.com>
2021-09-22 16:42:23 -0400

    3

--- page-59-dataframe-reader.py
+++ page-59-dataframe-reader.py
@@ -34,5 +34,5 @@ fire_schema = StructType([StructField('CallNumber', IntegerType(), True),
                           StructField('RowID', StringType(), True),
                           StructField('Delay', FloatType(), True)])
 # Use the DataFrameReader interface to read a CSV file
-sf_fire_file = "/root/LearningSparkV2/databricks-datasets/learning-spark-v2/sf-fire/sf-fire-calls.csv"
+sf_fire_file = "gs://wf-ae-hive-warehouse-prod/tmp/test201/LearningSparkV2/databricks-datasets/learning-spark-v2/sf-fire/sf-fire-calls.csv"
 fire_df = spark.read.csv(sf_fire_file, header=True, schema=fire_schema)
